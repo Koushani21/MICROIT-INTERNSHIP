799022b2b9d5d65676556a226fd6e5e7b4bd6a11
Koushani21 <koushanichandra555@gmail.com>
2025-05-15 15:10:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault(); // Prevent default form submission
+  e.preventDefault(); 
   alert('Thank you for your message! I will get back to you soon.');
-  this.reset(); // Clear the form
+  this.reset(); 
 });
